0bf8accf863977c5de765beb6015c3f9649b1dd1
sauravsuman7 <sauravsrivastava121@gmail.com>
2020-12-21 19:03:16 +0530

    is internet connection is on your phone

--- app/src/main/java/com/jlcsoftware/bloodbankcommunity/ChatApplication/ChatActivity.java
+++ app/src/main/java/com/jlcsoftware/bloodbankcommunity/ChatApplication/ChatActivity.java
@@ -308,6 +308,7 @@ public class ChatActivity extends AppCompatActivity implements MessageClickListe
         });
 
 
+        //hi
 
 
         chat_attachment.setOnClickListener(new View.OnClickListener() {
